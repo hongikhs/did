27795db65ee61a18e8ef8323f06b7840621b0c84
hongikhs <sw@hongik.hs.kr>
2018-08-30 21:49:08 +0900

    Update did2_rclone_omx.py

--- did2_rclone_omx.py
+++ did2_rclone_omx.py
@@ -8,6 +8,7 @@ import os
 delay = 3000
 root = Tk()
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+factor_screen = w / h
 #root.overrideredirect(True)
 root.geometry("%dx%d+0+0" % (w, h))
 #root.focus_set()
@@ -23,8 +24,17 @@ im_files = []
 for f in files:
     if f.lower().endswith('.jpg') or f.lower().endswith('.png'):
         im_files.append(f)
-#print(im_files)
-im = ImageTk.PhotoImage(Image.open(im_files.pop(0)).resize((w,h),Image.ANTIALIAS))
+f = im_files.pop(0)
+pic = Image.open(f)
+pic_w, pic_h = pic.size
+factor_pic = pic_w / pic_h
+if factor_pic > factor_screen:
+    disp_w = w
+    disp_h = pic_h * disp_w / pic_w
+else:
+    disp_h = h
+    disp_w = pic_w * disp_h / pic_h
+im = ImageTk.PhotoImage(pic.resize((disp_w,disp_h),Image.ANTIALIAS))
 la = Label(root, image=im, bg='black')
 #lb = label(root, text = '홍대부고 SW교육봉사 동아리')
 la.pack()
@@ -41,11 +51,21 @@ def im_update():
         print(im_files)
     try:
         f = im_files.pop(0)
-        if f.endswith('.mp4'):
+        print(f, len(im_files))
+        if f.lower().endswith('.mp4'):
             os.system('omxplayer -b ' + f)
             root.after(0, im_update)
         else:
-            im = ImageTk.PhotoImage(Image.open(f).resize((w,h),Image.ANTIALIAS))
+            pic = Image.open(f)
+            pic_w, pic_h = pic.size
+            factor_pic = pic_w / pic_h
+            if factor_pic > factor_screen:
+                disp_w = w
+                disp_h = pic_h * disp_w / pic_w
+            else:
+                disp_h = h
+                disp_w = pic_w * disp_h / pic_h
+            im = ImageTk.PhotoImage(pic.resize((disp_w,disp_h),Image.ANTIALIAS))
             la.configure(image=im)
             la.image = im
             root.after(delay, im_update)
